9f76b8217dac93d6bc1e995dc6cab3eab505bd23
MrZoDo <PetcuAdi@google.com>
2025-10-21 22:51:14 +0300

    Update after merge

--- routes/widget.js
+++ routes/widget.js
@@ -5,11 +5,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-<<<<<<< HEAD
     console.log('Am primit get pe widget');
-    res.render('mirror',{});
-=======
-    console.log('am primit get pe widget');
     //const widgetData = {
     //    TEMP: '22°C',
     //    // Add other data as needed
@@ -20,6 +16,5 @@ router.get('/', function(req, res, next) {
     //res.render('wg_temp_1',{Temperatura: '25'});
     //res.render('wg_temp_1');
     res.render('widgets/wg_temp',{ReadTempESP1: '10', ReadTempESP2: '20'});
->>>>>>> test
 });
 module.exports = router;
